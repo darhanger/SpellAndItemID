c12e0f74e7e52ae1b021c19e58f3420a1cc4da52
DarhangeR <civayan@gmail.com>
2021-06-25 08:19:58 +0300

    SpellAndItemID v1.2
    - Fix English;

---
+++ SpellAndItemID/Locales/Locale-enUs.lua
@@ -0,0 +1,6 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("SpellAndItemID", "enUS")
+if not L then return end
+
+L["ItemID:"] = "ItemID:"
+L["Applied by:"] = "Applied by:"
+L["SpellID:"] = "SpellID:"
